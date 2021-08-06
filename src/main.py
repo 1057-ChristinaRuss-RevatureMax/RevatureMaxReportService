593e576134eeb862265208f2d12316711f0ec79f
j-dax <67487509+j-dax@users.noreply.github.com>
2021-08-06 13:50:07 -0700

    renamed db_config function to smth more sensible

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # routes need to be touched by import once to proc the app import/setup
-# from src.config.db_config import reset_database
+# from src.config.db_config import execute_sql
 from src.route import *
 from src.config.flask_config import app
 from dotenv import dotenv_values
@@ -8,4 +8,4 @@ from dotenv import dotenv_values
 if __name__ == "__main__":
     dotenv_values("../.env")
     app.run()
-    # reset_database("src/sql/setup.sql")
+    # execute_sql("src/sql/setup.sql")
